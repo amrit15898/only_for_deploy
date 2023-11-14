c19beef35c69de474f4a49ed729860e9a35012a3
amrit15898 <apalsingh1581998@gmail.com>
2023-11-14 11:47:04 +0530

    change in setting.py file

--- only_for_deploy/settings.py
+++ only_for_deploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-emz!_dues%$j%w$u78)$upl%1rra$c8o_*1#l&%%u)%zb_iow^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.226.140.170"]
 
 
 # Application definition
